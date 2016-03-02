91c5570b58c7fc26afb04ee14917cd016f1bae5e
Christian Stein <sormuras@gmail.com>
2016-03-02 16:35:47 +0100

    Updated to latest stash snapshot.

--- treasure-demo/src/demo/com/github/sormuras/treasure/demo/AppenableDemo.java
+++ treasure-demo/src/demo/com/github/sormuras/treasure/demo/AppenableDemo.java
@@ -2,24 +2,23 @@ package com.github.sormuras.treasure.demo;
 
 import java.nio.ByteBuffer;
 
-import com.github.sormuras.stash.ProxyStash;
-
+import com.github.sormuras.stash.Stash;
 
 public class AppenableDemo {
-  
+
   public static void main(String[] args) throws Exception {
     ByteBuffer buffer = ByteBuffer.allocate(1000);
-    Appendable appendable = ProxyStash.create(Appendable.class, new StringBuilder(), buffer);
+    Appendable appendable = Stash.proxy(Appendable.class, new StringBuilder(), buffer);
     appendable.append('@').append("abc").append("abcdef", 3, 6);
-    //assertEquals("@abcdef", appendable.toString());
+    // assertEquals("@abcdef", appendable.toString());
     buffer.flip();
-    appendable = ProxyStash.create(Appendable.class, new StringBuilder(), buffer);
-    //assertEquals("@abcdef", appendable.toString());
+    appendable = Stash.proxy(Appendable.class, new StringBuilder(), buffer);
+    // assertEquals("@abcdef", appendable.toString());
     appendable.append('@');
-    //assertEquals("@abcdef@", appendable.toString());
+    // assertEquals("@abcdef@", appendable.toString());
     buffer.flip();
-    appendable = ProxyStash.create(Appendable.class, new StringBuilder(), buffer);
-    //assertEquals("@abcdef@", appendable.toString());
+    appendable = Stash.proxy(Appendable.class, new StringBuilder(), buffer);
+    // assertEquals("@abcdef@", appendable.toString());
     System.out.println(appendable);
   }
 
