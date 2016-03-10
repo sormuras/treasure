b0b0acb1b01f93077ad99277b47bbc86c749e4dc
Christian Stein <sormuras@gmail.com>
2016-03-10 07:40:49 +0100

    More club demo lines.

--- treasure-demo/src/club/com/github/sormuras/treasure/club/Club.java
+++ treasure-demo/src/club/com/github/sormuras/treasure/club/Club.java
@@ -3,6 +3,7 @@ package com.github.sormuras.treasure.club;
 import java.nio.ByteBuffer;
 
 import com.github.sormuras.stash.Stash;
+import com.github.sormuras.stash.Treasure;
 
 @Stash.Interface
 public interface Club extends AutoCloseable {
@@ -14,21 +15,24 @@ public interface Club extends AutoCloseable {
   void rename(int number, String newName);
 
   static void main(String[] args) throws Exception {
-    ByteBuffer tmp1 = ByteBuffer.allocate(1000);
-    try (Club club = Stash.create(Club.class, new Disco(), tmp1)) {
+    ByteBuffer buffer = ByteBuffer.allocate(1000);
+    try (Club club = Stash.create(Club.class, new Disco(), buffer)) {
       club.create("Aaron");
       club.create("Bert");
       club.create("Carla");
       club.rename(1, "Bartholomaeus");
     }
-    ByteBuffer tmp2 = ByteBuffer.allocate(1000);
-    try (Club club = Stash.proxy(Club.class, new Disco(), tmp2)) {
-      club.create("Aaron");
-      club.create("Bert");
-      club.create("Carla");
-      club.rename(1, "Bartholomaeus");
+    try (Club club = Stash.proxy(Club.class, new Disco(), buffer)) {
+      assert "Aaron".equals(club.member(0).name());
+      assert "Bartholomaeus".equals(club.member(1).name());
+      assert "Carla".equals(club.member(2).name());
+      club.rename(2, "Ceera");
     }
-    System.out.println(tmp1.equals(tmp2));
+    try (Club club = new ClubStash(Treasure.create(new Disco(), buffer))) {
+      assert "Aaron".equals(club.member(0).name());
+      assert "Bartholomaeus".equals(club.member(1).name());
+      assert "Ceera".equals(club.member(2).name());
+    }
   }
 
 }
